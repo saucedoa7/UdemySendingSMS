8a2f557c318bb10284e87071a1adcb7a21c9008b
Albert Saucedo <albert@Mac-mini.local>
2015-02-05 21:58:44 -0600

    Initial Commit

---
+++ SendingMSM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SendingMSM
+//
+//  Created by Albert Saucedo on 2/5/15.
+//  Copyright (c) 2015 Albert Saucedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SendingMSM/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SendingMSM
+//
+//  Created by Albert Saucedo on 2/5/15.
+//  Copyright (c) 2015 Albert Saucedo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
